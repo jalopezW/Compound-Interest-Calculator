e76defe450b48ecddfb89d8df77db8f6628580ce
Jesus <jlopezloyolamarymount@gmail.com>
2024-11-18 12:45:11 -0800

    Pushing interest calculator HW

---
+++ README.md
@@ -0,0 +1,4 @@
+# Interest Calculator
+A simple, static webapp in which users can enter principal, APR, times per year that the interest is applied, the number of years, and it tells you the new principal
+
+Run the app [here](https://2dwnqf.csb.app/).
---
+++ compound_interest.js
@@ -0,0 +1,51 @@
+const principalInput = document.querySelector("#principal");
+const aprSlider = document.querySelector("#apr");
+const aprOutput = document.querySelector("#aprOutput");
+const frequencyInput = document.querySelector("#frequency");
+const yearsInput = document.querySelector("#years");
+const accumulatedOutput = document.querySelector("#accumulated");
+const totalOutput = document.querySelector("#total");
+
+//Update APR output on slider input
+aprSlider.addEventListener("input", () => {
+  aprOutput.textContent = `${aprSlider.value}%`;
+  calculate();
+});
+
+//Recalculate when inputs change
+[principalInput, frequencyInput, yearsInput].forEach((input) => {
+  input.addEventListener("input", calculate);
+});
+
+function calculate() {
+  //Get values from inputs
+  const principal = parseFloat(principalInput.value); //parseFloat func converts strings to floats
+  const apr = parseFloat(aprSlider.value) / 100; //Convert percentage to decim
+  const frequency = parseFloat(frequencyInput.value);
+  const years = parseFloat(yearsInput.value);
+
+  //Check if inputs are valid, but not necessary bc number input fields
+  if (
+    isNaN(principal) ||
+    isNaN(apr) ||
+    isNaN(frequency) ||
+    isNaN(years) ||
+    principal <= 0 ||
+    frequency <= 0 ||
+    years <= 0
+  ) {
+    accumulatedOutput.textContent = "$0.00";
+    totalOutput.textContent = "$0.00";
+    return; //If any of the inputs are invalid, the interest and total amount outputs are set to "$0.00"
+  }
+
+  //Compound interest calculation
+  const accumulatedInterest =
+    principal * Math.pow(1 + apr / frequency, frequency * years) - principal; //(base, exponent) also interest earned
+  const totalAmount = principal + accumulatedInterest;
+  //final_amount = p * ((1 + (r/n))**(n*t))
+
+  //Display results
+  accumulatedOutput.textContent = `$${accumulatedInterest.toFixed(2)}`;
+  totalOutput.textContent = `$${totalAmount.toFixed(2)}`;
+}
